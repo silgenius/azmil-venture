73b7af8809c598848306a1d20b762b1d2d9421cf
silgenius <olutademartin@gmail.com>
2026-03-19 14:54:26 +0100

    feat: Add the store provider to app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import Header from "@/src/components/Header";
+import { AppProvider } from "@/src/store/AppProvider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -28,8 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
-        {children}
+        <AppProvider>
+          <Header />
+
+          {children}
+        </AppProvider>
       </body>
     </html>
   );
