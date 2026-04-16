91d109ab3564dc35f98265d9f6bf48dfc9edb098
silgenius <olutademartin@gmail.com>
2026-04-16 17:49:30 +0100

    fix: css crash

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { AppProvider } from "@/src/store/AppProvider";
 import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -82,7 +83,6 @@ export default function RootLayout({
           <Header />
           {children}
           <Footer />
-
           <Analytics />
 
           <script
